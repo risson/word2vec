e45ed71a3f8a98559f9e5c78ffdbd2b723c7c3ac
risson <risson@rissons-MacBook-Pro.local>
2018-06-24 23:23:32 +0800

    Add functions

--- utils.py
+++ utils.py
@@ -1,41 +1,117 @@
 import numpy as np
 import random
+import re
 
 
-def preprocess_corpus_file(corpus_file, separator=" ", punct_kept=False, lowfreq_unk_thres=3, highfreq_subsampling=False,
-                           subsampling_t=1e-5, subsampling_thres=0.8):
+def preprocess_corpus_file(corpus_file, lowfreq_unk_thres=0):
     """
     """
+    word_to_id = {}
+    id_to_word = {}
+    word_count = {}
+    total_cnt = 0
     with open(corpus_file) as fopen:
-        
-        word_to_id = {}
-        id_to_word = {}
-        word_count = {}
-        total_cnt = 0
         for line in fopen:
-            for word in line.strip().split(separator):
-                if punct_kept:
-                    print("Currently not keeping punctuation. This function will be built later.")
-                else:
-                    word = word.lower().strip().split(',')[0]
-                    word = word.strip().split('.')[0]
-                    word = word.strip().split('！')[0]
-                    word = word.strip().split('[')[0]
-                    word = word.strip().split(']')[0]
-                    print(word)
+            line = re.sub("[\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+", "",line)
+            for word in line.strip().split(" "):
+                if word is "":
+                    continue
+                word = word.lower()
+                if word not in word_count:
+                    word_count[word] = 0
+                word_count[word] += 1
+                total_cnt += 1
+
+    # Make some low frequency words into unknown <UNK>
+    word_count = dict(filter(lambda _:_[1]>=lowfreq_unk_thres, word_count.items()))
+    vocab_size = len(word_count)
+    word_to_id = dict(zip(sorted(list(word_count.keys())), range(vocab_size)))
+    new_cnt = sum(_[1] for _ in word_count.items())
+    word_to_id['<UNK>'] = vocab_size
+    word_count['<UNK>'] = total_cnt - new_cnt
+    id_to_word = dict((value, key) for key, value in word_to_id.items())
+
+    return word_to_id, id_to_word, word_count, vocab_size
+
+def negative_sample_array(word_id_dict, word_count, negative_array_size=1e8):
+    """
+    """
+    word_count_refine = dict((_[0],_[1]**0.75) for _ in word_count.items())
+    refine_cnt = sum(_[1] for _ in word_count_refine.items())
+    word_freq = dict((_[0], _[1]/refine_cnt) for _ in word_count_refine.items())
+
+    negative_array = []
+
+    i = 0    
+    for word, freq in word_freq.items():
+        if i >= negative_array_size: break
+        word_num = round(freq*negative_array_size)
+        for _ in range(word_num):
+            if i >= negative_array_size: continue
+            negative_array.append(word)
+            i += 1
+
+    return negative_array
+
+def negative_sample_generate(negative_array):
+    """
+    """
+    index = random.randint(0, len(negative_array))
+    return negative_array[index]
+
+def corpus_reader(corpus_file, word_count, word_id_dict, context_window, random_window_size,
+                  subsampling_t, subsampling_thres, negative_sample_size, negative_array):
+    """
+    """
+    def reader():
+        with open(corpus_file) as fopen:
+            for line in fopen:
+                line = re.sub("[\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+", "",line)
+                line_list = line.strip().split(" ")
+                word_ids = [word_id_dict.get(_.lower(), word_id_dict.get("<UNK>")) for _ in line_list]
+
+                if random_window_size:
+                    cur_window = random.randint(1, context_window)
+                
+                for i in range(len(word_ids)):
+                    target = word_ids[i]
+                    freq = word_count[target]
+                    if discard_word(subsampling_t, subsampling_thres, freq):
+                        continue
+                    
+                    context_list = []
+                    context_list.append(target)
+                    # generate positive sample
+                    for j in range(i-cur_window, i+cur_window):
+                        if j<0 or j==i:
+                            continue
+                        pos_context = word_ids[j]
+                        context_list.append(pos_context)
+                        yield ((target, pos_context),1)
+                    
+                    # generate negative sample
+                    for _ in range(cur_window*2*negative_sample_size):
+                        neg_context = negative_sample_generate(negative_array)
+                        while neg_context in context_list:
+                            neg_context = negative_sample_generate(negative_array)
+                        yield ((target, neg_context),0)
+    return reader
+                        
+                        
+
+                    
 
 
-def negative_sample_generate():
-    """
-    """
 
-def negative_sample_shuffle():
-    """
-    """
+def discard_word(t, threshold, freq):
+    p_discard = 1 - (t/freq)**0.5
+    if p_discard >= threshold:
+        return True
 
-def corpus_reader():
-    """
-    """
+
 
 if __name__ == "__main__":
-    preprocess_corpus_file("c:/Users/risson.yao/Documents/AI Edu/NLP/simpletest.txt")
+    a,b,c,d = preprocess_corpus_file("/Users/risson/git/word2vec/testpage.txt")
+    print(a)
+    print(b)
+    print(c)
--- word2vec.py
+++ word2vec.py
@@ -15,42 +15,61 @@ class SkipGramNS(object):
     Implementing word2vec using Skip-Gram and Negative Sampling
     """
 
-    def __init__(self, corpus_file, separator=" ", punct_kept=False, lowfreq_unk_thres=3, highfreq_subsampling=False, 
-    subsampling_t=1e-5,  subsampling_thres=0.8,embed_dim=300, window_size=2, optimizer="adam", loss="binary_crossentropy"):
+    def __init__(self, corpus_file, lowfreq_unk_thres=3, 
+                 embed_dim=300, window_size=2, optimizer="adam", loss="binary_crossentropy"):
         """
         corpus_file: the file that contains the corpus
-        separator: word separator
-        punct_kept: decide whether to keep punctuation 
         lowfreq_unk_thres: the threshold to deside whether a low frequency word should be replaced by '<UNK>'
-        highfreq_subsampling: whether we want to remove words with high frequency
-        subsampling_t: the t value to calculate probability of a word to be removed
-        subsampling_thres: the threshold to calculate probability of a word to be removed 
         embed_dim: the dimension of the output word2vec
         window_size: size of the Skip_Gram window for context
-        batch_size: size of the batch for 1 forward feeding and backprop
-        epoches: number of epoches. In 1 epoch we will process the entire input data set
         optimizer: define the method for optimization. Possible values are: "adam", "RMSprop", "momentum"...
         loss: define the loss function. Possible values are: "binary_crossentropy", "hinge"...
         """
         self._corpus_file = corpus_file
-        self._embed_dim = embed_dim
-        self._word_to_id, _id_to_word, word_count = utils.preprocess_corpus_file(corpus_file, separator, lowfreq_unk_thres, highfreq_subsampling,
-                                                                                 subsampling_t, subsampling_thres)
-        self._model = self.__init_model__(optimizer, loss)
+        self._word_to_id, _id_to_word, word_count, _vocab_size = utils.preprocess_corpus_file(corpus_file, separator, punct_kept, lowfreq_unk_thres)
+        self._model = self.__init_model__(self._vocab_size, embed_dim, optimizer, loss)
 
-    def __init_model__(self, optimizer, loss):
+    def __init_model__(self, vocab_size, embed_dim, optimizer, loss):
         """
 
         """
+        target = tf.keras.Input(shape=(self._vocab_size,), name="target")
+        context = tf.keras.Input(shape=(self._vocab_size,), name="context")
+
+        model = MySkipGramNSModel(inputs=[target,context])
+        model.compile(optimizer=optimizer, loss=loss)
+        return model
     
-    def train(self, epoches=10, batch_size=512, negative_sample_size=1):
+    def train(self, epoches=10, batch_size=512, negative_sample_size=1, context_window=2, random_window_size=True,
+              subsampling_t=1e-5,  subsampling_thres=0.8, negative_sample_size=1):
         """
+        epoches: number of epoches. In 1 epoch we will process the entire input data set
+        batch_size: size of the batch for 1 forward feeding and backprop
         negative_sample_size:   negative sample size, which is k times bigger than positive sample size. 
                                 If set as 0, then no negative samples. If set as 1, then same size as positive samples
+        highfreq_subsampling: whether we want to remove words with high frequency
+        subsampling_t: the t value to calculate probability of a word to be removed
+        subsampling_thres: the threshold to calculate probability of a word to be removed 
         """
+        for epoch in range(epoches):
+            for batch in range(batch_size):
+
     
     def return_word_vec(self):
         """
 
         """
 
+class MySkipGramNSModel(tf.keras.Model):
+
+    def __init__(self):
+        self.shared_embed = tf.keras.layers.Embedding(vocab_size, embed_dim, input_length=1, name="shared_embed")
+        self.reshaped_layer = tf.keras.layers.Reshape((1,), input_shape(1,1))
+        self.sigmoid_dense = tf.keras.layers.Dense(1, input_shape=(1,) activation="sigmoid")
+
+    def call(self, inputs):
+        target_embed = self.shared_embed(inputs[0])
+        context_embed = self.shared_embed(inputs[1])
+        merged_vector = tf.keras.layers.dot([target_embed, context_embed], axes=-1)
+        reshaped_vector = self.reshaped_layer(merged_vector)
+        return sigmoid_dense(reshaped_vector)
